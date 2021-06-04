ff4070bd62263af8a0d80f06ca5098943ff127df
mrclalmda <marcelo_almeida@live.com>
2021-06-04 08:16:23 -0300

    Projeto 1.0

--- homebroker/src/app/app.module.ts
+++ homebroker/src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { SharedModule } from './shared/shared.module';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
+registerLocaleData(localePt);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    DashboardModule,
-    SharedModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, DashboardModule, SharedModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- homebroker/src/app/dashboard/dashboard-page/dashboard-page.component.ts
+++ homebroker/src/app/dashboard/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,23 @@
-import { OnInit, Component } from '@angular/core';
+import { OnInit, Component, Input } from '@angular/core';
+import Stock from 'src/app/shared/models/stock-model';
+import { DashboardService } from '../dashboard.service';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
-  styleUrls: ['./dashboard-page.component.css']
+  styleUrls: ['./dashboard-page.component.css'],
 })
 export class DashboardPageComponent implements OnInit {
 
-  constructor() { }
+  stocks: Stock[] = [];
 
-  ngOnInit(): void {
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit(): void {this.fetchStocks()}
+
+  async fetchStocks(): Promise<void> {
+    this.stocks = await this.dashboardService.getStocks();
+    console.log(this.stocks);
+
   }
-
 }
